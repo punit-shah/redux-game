789960f6f52bfdedd4cdf23631dad7c6bfa18801
Punit Shah <punit.shah@bbc.co.uk>
2019-05-03 12:31:57 +0100

    Remove CurrentScore shadow filter

--- src/components/CurrentScore.js
+++ src/components/CurrentScore.js
@@ -2,12 +2,7 @@ import React from 'react';
 import { number } from 'prop-types';
 
 const CurrentScore = ({ score }) => (
-  <g filter="url(#shadow)">
-    <defs>
-      <filter id="shadow">
-        <feDropShadow dx={1} dy={1} stdDeviation={5} />
-      </filter>
-    </defs>
+  <g>
     <text
       style={{ fontFamily: 'Consolas, monospace', fontSize: 30, fill: '#fff' }}
       x={-window.innerWidth / 2 + 20}
